fbbe9eca387e6eef002b9166fcebb1b3a42247ee
Omkar Singh <omkarsinghaws@gmail.com>
2024-08-17 22:26:05 +0530

    Base project

---
+++ src/main/java/com/oks/git/learn/GitLearningApplication.java
@@ -0,0 +1,13 @@
+package com.oks.git.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oks/git/learn/GitLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oks.git.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
