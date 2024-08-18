6e191a77aa753832142ea2dce6906c2fd3407ba3
Omkar Singh <omkarsinghaws@gmail.com>
2024-08-18 18:08:06 +0530

    Adding method05 from gw04

--- src/main/java/com/oks/git/learn/bean/Common.java
+++ src/main/java/com/oks/git/learn/bean/Common.java
@@ -10,5 +10,9 @@ public class Common {
 	public void method02() {
 		System.out.println("Dev02 Branch: method 2");
 	}
+	
+	public void method05() {
+		System.out.println("gw04 workspace : method 5");
+	}
 
 }
